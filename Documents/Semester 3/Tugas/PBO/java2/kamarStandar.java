9a5fa9f10b10a21f5f9e7190dac920373e5d9bed
MAlvanAzizWirdian435 <149524934+MAlvanAzizWirdian435@users.noreply.github.com>
2024-12-15 20:54:52 +0700

    Delete Documents/Semester 3/Tugas/PBO/java2 directory

--- Documents/Semester 3/Tugas/PBO/java2/kamarStandar.java
+++
@@ -1,6 +0,0 @@
-public class kamarStandar extends kamar {
-    public kamarStandar(int noRuang) {
-        super(noRuang, 150000);
-    }
-    
-}
--- Documents/Semester 3/Tugas/PBO/java2/kamarSuite.java
+++
@@ -1,6 +0,0 @@
-public class kamarSuite extends kamar {
-    public kamarSuite(int noRuang) {
-        super(noRuang, 250000);
-    }
-    
-}
